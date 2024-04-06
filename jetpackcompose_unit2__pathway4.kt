68fb2bb94ce3d5bcb3f5e9d8eec455f7a860c0d6
佐野澄哉 <sano@sanosumiyas-MacBook-Pro.local>
2024-04-07 00:22:15 +0900

    jetpackcomposeの勉強。kotlin基礎学習コード

---
+++ jetpackcompose_unit2__pathway4.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val treatFunction = trickOrTreat(false) {"$it quarters"}
+    val trickFunction = trickOrTreat(true, null)
+    repeat(4){
+        treatFunction()
+    }
+    trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit{
+    if(isTrick){
+        return trick
+    } else{
+        if(extraTreat != null){
+			println(extraTreat(5))            
+        }
+        return treat
+    }
+}
+
+val trick = {
+    println("trick!")
+}
+
+val treat:() -> Unit ={
+    println("treat!")
+}
